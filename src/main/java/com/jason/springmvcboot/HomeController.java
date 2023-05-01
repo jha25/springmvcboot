bed3e421eacf1ea16d4ad18a556a0451a4354820
Ha <630019@bah.com>
2023-05-01 15:24:47 -0500

    added @ModelAttribute to incorporate Alien class

--- src/main/java/com/jason/springmvcboot/HomeController.java
+++ src/main/java/com/jason/springmvcboot/HomeController.java
@@ -1,25 +1,38 @@
 package com.jason.springmvcboot;
 
+import com.jason.springmvcboot.model.Alien;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+    @ModelAttribute
+    public void modelData(Model m){
+        m.addAttribute("name", "Aliens");
+    }
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session){
+    public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, Model m){
         int num3 = i + j;
 
-        session.setAttribute("num3", num3);
+        m.addAttribute("num3", num3);
 
-        return "result.jsp";
+        return "result";
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien(@ModelAttribute("a1") Alien a){
+        return "result";
     }
 }
